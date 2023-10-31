2db05d4a3ee01f3ae211a764df971b8ee7d2c55d
Jonathan Cross <118997615+JonathanAlbertCross@users.noreply.github.com>
2023-10-31 22:29:46 +0000

    map visual

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,55 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [location, setLocation] = useState({});
+  const [search, setSearch] = useState("");
+  const [number, setNumber] = useState(10);
+
+  function handleChange(event) {
+    setSearch(event.target.value);
+  }
+
+  async function getLocation(event) {
+    event.preventDefault();
+
+    // the API url we are going to make a request to
+    const API = `https://eu1.locationiq.com/v1/search?q=${search}&key=${API_KEY}&format=json`;
+
+    // make the GET request
+    const res = await axios.get(API);
+
+    // set location to be our response
+    setLocation(res.data[0]);
+  }
+
+  function handleNumber(mod) {
+    setNumber(number + mod);
+  }
 
   return (
     <>
+      <h1>Map App</h1>
       <form onSubmit={getLocation}>
-        <input type="text" />
+        <input onChange={handleChange} placeholder="Location" />
         <button>Get Location</button>
       </form>
+      {location.lat && (
+        <div>
+          <button onClick={() => handleNumber(-1)}>-</button>
+          <span>{number}</span>
+          <button onClick={() => handleNumber(1)}>+</button>
+
+          <img
+            src={`https://maps.locationiq.com/v3/staticmap?key=${API_KEY}&center=${location.lat},${location.lon}&zoom=${number}&format=png`}
+          />
+        </div>
+      )}
+
+      <h2>{location.display_name}</h2>
     </>
   );
 }
